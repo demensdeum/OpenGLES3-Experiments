a9cfafefcd00b2a5d184787732e0a2944434583e
demensdeum <demensdeum>
2017-07-23 10:44:11 +0400

    Assimp obj loader, textures, rotation

---
+++ 7-sdl-gles-obj-textured-assimp/FSGL/Data/Model/FSGLModel.h
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FSEOGLModel.h
+ * Author: demensdeum
+ *
+ * Created on July 8, 2017, 10:39 AM
+ */
+
+#ifndef FSEOGLMODEL_H
+#define FSEOGLMODEL_H
+
+#include <memory>
+
+#include "../Resource/FSGLResource.h"
+
+#include <GLES3/gl3.h>
+
+#include <vector>
+
+using namespace std;
+
+class FSGLModel: public FSGLResource {
+    
+public:
+    FSGLModel();
+    FSGLModel(const FSGLModel& orig);
+    virtual ~FSGLModel();
+    
+    GLfloat* glVertices();
+    GLushort* glIndices();
+    
+    vector<GLfloat> vertices;
+    vector<GLushort> indices;
+    vector<GLushort> uv;
+    
+    const int vertexSize = 5;
+    
+private:
+    
+    GLushort *_glIndices;
+    GLfloat *_glVertices;
+};
+
+#endif /* FSEOGLMODEL_H */
+
---
+++ 7-sdl-gles-obj-textured-assimp/FSGL/Data/Resource/FSGLResource.h
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   FSGLResource.h
+ * Author: demensdeum
+ *
+ * Created on July 9, 2017, 10:22 AM
+ */
+
+#ifndef FSGLRESOURCE_H
+#define FSGLRESOURCE_H
+
+class FSGLResource {
+public:
+    FSGLResource();
+    FSGLResource(const FSGLResource& orig);
+    virtual ~FSGLResource();
+private:
+
+};
+
+#endif /* FSGLRESOURCE_H */
+
